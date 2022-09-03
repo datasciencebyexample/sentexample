bc725fb4d3cfdc4a941388e39a05066a907c23d4
datasciencebyexample <charleyliu@gmail.com>
2022-09-03 11:50:46 -0400

    change

--- static/js/index.js
+++ static/js/index.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
 
 	}
 	else{
-	    searchExample(keyword);
+	    //searchExample(keyword);
+		recommendExample(keyword);
 	}
 	
     });
@@ -30,14 +31,16 @@ $(document).keyup(function (event) {
 			recommend($("#keyword").val());
 	}
 	else{
-	    searchExample($("#keyword").val());
+	    //searchExample($("#keyword").val());
+		recommendExample($("#keyword").val());
 	}
     }
 });
 
 
 function recommend(key) {
-  // Grab the values from the input
+    // 首先清空result中的内容以便内容填入
+    $("#result").empty();
   
   // call api
   // POST request using fetch()
@@ -65,18 +68,14 @@ function recommend(key) {
 	 
 	.then(function(result)
 	  {
-	   console.log(result)
+	   //console.log(result)
 	   var result = $.parseJSON(result)
-	   console.log(result)
 	  
-	  //do stuff
-	    console.log(result['results'])
-		console.log(result.results)
 		let res = result['results'];
 		
 		
 		// 利用for插入每一个结果
-		if (result['results'].length) {
+		if (res.length) {
 			for (i = 0; i < res.length; i++) {
 				// 将返回的结果包装成HTML
 				resultItem =
@@ -118,6 +117,93 @@ function recommend(key) {
   
 }
 
+
+
+
+function recommendExamplekey) {
+    // 首先清空result中的内容以便内容填入
+    $("#result").empty();
+  
+  // call api
+  // POST request using fetch()
+	fetch("https://7vbxmqkrzg.execute-api.us-east-1.amazonaws.com/prod/", {
+		
+		mode: "cors",
+		// Adding method type
+		method: "POST",
+		
+		// Adding body or contents to send
+		body: JSON.stringify({
+			"index":"example",
+			"data":{"query":key}
+			
+		}),
+		 
+		// Adding headers to the request
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+	 
+	// Converting to JSON
+	.then(response => response.json())
+	 
+	.then(function(result)
+	  {
+	   //console.log(result)
+	   var result = $.parseJSON(result)
+	  
+		let res = result['results'];
+		
+		
+            // 利用for插入每一个结果
+            if (res.length) {
+                for (i = 0; i < res.length; i++) {
+                    // 将返回的结果包装成HTML
+                    resultItem =
+                        `
+                        <div class='col-md-12 mb-4'>
+                            <div class='card mb-12 shadow-sm'>
+                                <div class='card-body'>
+                                    <h5>` +
+                        //"Sentence example"  +
+		
+                       `</small></h5>`+
+                       //             <p class='text-muted' style='margin-bottom: 0.5em'>` + "Highlight: " +
+                       // res[i].highlight +
+			// `</p>
+
+		    `<p class='card-text'>` +
+			//"<p style='background-color:#fff8dc;'> Highlight: </p>"+
+                        res[i].highlight+
+                        `</p>`+
+			//`<p class='card-text'>` + "<p style='background-color:#fff8dc;'> More: </p>"+
+                        //res[i].content+
+                        //`</p>`+
+                               `</div>
+                            </div>
+                        </div>
+                    `;
+                    // 插入HTML到result中
+                    $("#result").append(resultItem);
+                }
+
+                // 搜索完以后让搜索框移上去，带有动画效果
+                $("section.jumbotron").animate({
+                    margin: "0"
+                });
+                // 显示搜索结果的部分
+                $("#resultSection").show();
+                // 清空输入联想
+                $("#suggestList").empty();
+            }
+	  
+	}).catch(error => console.error('Error:', error)); 
+  
+	//console.log(results);
+  
+  
+}
 
 
 
