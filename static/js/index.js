5e2ee11d8eeb57aff80f0724c4c5442b267988cd
datasciencebyexample <charleyliu@gmail.com>
2022-09-03 10:49:09 -0400

    update headers

--- static/js/index.js
+++ static/js/index.js
@@ -39,7 +39,7 @@ function searchPaper2(key) {
     // 首先清空result中的内容以便内容填入
     $("#result").empty();
     $.getJSON({
-        url: "https://7vbxmqkrzg.execute-api.us-east-1.amazonaws.com/prod/",
+        url: "https://7vbxmqkrzg.execute-api.us-east-1.amazonaws.com/prod/?callback=",
 	data: {'index':'word','data':{'query':key}},
 	
 	type: 'POST',
@@ -98,7 +98,8 @@ function searchExample(key) {
     // 首先清空result中的内容以便内容填入
     $("#result").empty();
     $.getJSON({
-        url: "https://7vbxmqkrzg.execute-api.us-east-1.amazonaws.com/prod/",
+        url: "https://7vbxmqkrzg.execute-api.us-east-1.amazonaws.com/prod/?callback=",
+		
 	data: {'index':'example','data':{'query':key}},
 	
 	type: 'POST',
